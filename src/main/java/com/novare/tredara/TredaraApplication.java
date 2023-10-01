6e1cfc172d3054469010d2d8d766a51693994c5c
Alireza Kafshdartoosi <71192730+AlirezaKToosi@users.noreply.github.com>
2023-10-01 13:47:35 +0200

    Initial commit

---
+++ src/main/java/com/novare/tredara/TredaraApplication.java
@@ -0,0 +1,13 @@
+package com.novare.tredara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TredaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TredaraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/novare/tredara/TredaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novare.tredara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TredaraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
