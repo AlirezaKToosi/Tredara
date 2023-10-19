46f12e1a644a2231674723963dc949292d23fb43
aggarrohit <32900818+aggarrohit@users.noreply.github.com>
2023-10-19 10:19:26 +0200

    added missing classes

---
+++ src/main/java/com/novare/tredara/security/userdetails/UserDetailsImpl.java
@@ -0,0 +1,100 @@
+package com.novare.tredara.security.userdetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.novare.tredara.models.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+
+public class UserDetailsImpl implements UserDetails {
+    private Long id;
+    private String email;
+
+    private String fullName;
+
+    @JsonIgnore
+    private String password;
+
+    private GrantedAuthority authority;
+
+    public UserDetailsImpl(Long id, String fullName, String email, String password,
+                           GrantedAuthority authority) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.authority = authority;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public static UserDetailsImpl build(User user) {
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().name());
+
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getFullName(),
+                user.getEmail(),
+                user.getPassword(),
+                authority);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(authority);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        UserDetailsImpl user = (UserDetailsImpl) o;
+        return Objects.equals(id, user.id);
+    }
+}
